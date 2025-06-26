11dbb67662228ef29fc9c8ac4f3c2984dff4f30f
rangachimalgi <ranganath.chimalgi@patientry.ai>
2025-06-26 18:41:41 +0530

    vite config

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
 }
 
