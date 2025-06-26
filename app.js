4ece08cd02a3e553cb35292f9a94f304424616c8
rangachimalgi <ranganath.chimalgi@patientry.ai>
2025-06-26 14:40:30 +0530

    backend setup added

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Admin backend is running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
